3a1309af7523883b1852dcde5485b4588d570e59
Pariterre <pariterre@hotmail.com>
2023-06-03 13:29:15 -0400

    Successfully selecting pause and active background image

--- lib/common/app_theme.dart
+++ lib/common/app_theme.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 class ThemeColor {
   static const Color greenScreen = Color.fromARGB(255, 0, 255, 0);
   static const Color main = Color.fromARGB(255, 45, 74, 168);
+  static const Color text = Colors.white;
   static const Color conceiled = Color.fromARGB(255, 45, 74, 168);
   static const Color conceiledContrast = Colors.amber;
   static const Color revealed = Color.fromARGB(255, 204, 234, 248);
+}
+
+class ThemeButton {
+  static ButtonStyle elevated =
+      ElevatedButton.styleFrom(backgroundColor: Colors.white);
 }
 
 class ThemeSize {
--- lib/providers/app_preferences.dart
+++ lib/providers/app_preferences.dart
@@ -10,38 +10,48 @@ import 'package:twitch_pomorodo_timer/common/config.dart';
 String _path(Directory directory, String filename) =>
     '${directory.path}/$filename';
 
+String get rootPath => Platform.isWindows ? r'C:\' : '/';
+
 class AppPreferences with ChangeNotifier {
   // Path to save data folder and file
-  final Directory _saveDirectory;
-  String get _preferencesPath => _path(_saveDirectory, preferencesFilename);
+  final Directory preferencesDirectory;
+  String get _preferencesPath =>
+      _path(preferencesDirectory, preferencesFilename);
+  Directory _lastVisitedDirectory = Directory('');
+  Directory get lastVisitedDirectory => _lastVisitedDirectory;
 
   // Background image during the countdown
-  String? _activeBackgroundImagePath;
-  String? get activeBackgroundImagePath => _activeBackgroundImagePath;
+  String? _activeBackgroundImageFilename;
+  String? get activeBackgroundImagePath =>
+      _activeBackgroundImageFilename == null
+          ? null
+          : _path(preferencesDirectory, _activeBackgroundImageFilename!);
   Future<void> setActiveBackgroundImagePath(String? value) async {
     if (value == null) {
-      _activeBackgroundImagePath = null;
+      _activeBackgroundImageFilename = null;
       notifyListeners();
     }
-    _activeBackgroundImagePath = await _copyFile(original: value!);
+    _activeBackgroundImageFilename = await _copyFile(original: value!);
     notifyListeners();
   }
 
   // Background image during the pause
-  String? _pauseBackgroundImagePath;
-  String? get pauseBackgroundImagePath => _pauseBackgroundImagePath;
+  String? _pauseBackgroundImageFilename;
+  String? get pauseBackgroundImagePath => _pauseBackgroundImageFilename == null
+      ? null
+      : _path(preferencesDirectory, _pauseBackgroundImageFilename!);
   Future<void> setPauseBackgroundImagePath(String? value) async {
     if (value == null) {
-      _pauseBackgroundImagePath = null;
+      _pauseBackgroundImageFilename = null;
       notifyListeners();
     }
-    _pauseBackgroundImagePath = await _copyFile(original: value!);
+    _pauseBackgroundImageFilename = await _copyFile(original: value!);
     notifyListeners();
   }
 
   ///
   /// Vanilla way to save the current preferences to a file
-  void savePreferences() async {
+  void save() async {
     final file = File(_preferencesPath);
     await file.writeAsString(json.encode(_serializePreferences));
   }
@@ -70,29 +80,43 @@ class AppPreferences with ChangeNotifier {
     }
     return AppPreferences._(
         directory: directory,
-        pomodoroRunningImagePath:
-            previousPreferences?['pomodoroRunningImagePath']);
+        activeBackgroundImageFilename:
+            previousPreferences?['activeBackgroundImageFilename'],
+        pauseBackgroundImageFilename:
+            previousPreferences?['pauseBackgroundImageFilename'],
+        lastVisitedDirectory: Directory(
+            previousPreferences?['lastVisitedDirectory'] ??
+                documentDirectory.path));
   }
 
   AppPreferences._({
     required Directory directory,
-    String? pomodoroRunningImagePath,
-  })  : _saveDirectory = directory,
-        _activeBackgroundImagePath = pomodoroRunningImagePath;
+    required String? activeBackgroundImageFilename,
+    required String? pauseBackgroundImageFilename,
+    required Directory lastVisitedDirectory,
+  })  : preferencesDirectory = directory,
+        _activeBackgroundImageFilename = activeBackgroundImageFilename,
+        _pauseBackgroundImageFilename = pauseBackgroundImageFilename,
+        _lastVisitedDirectory = lastVisitedDirectory;
 
   // INTERNAL METHODS
 
   ///
-  /// Copy a file and return the new path
+  /// Copy a file and return the name of the new file
   Future<String> _copyFile({required String original}) async {
     final file = File(original);
-    final newFile = await file.copy(_path(_saveDirectory, basename(file.path)));
-    return newFile.path;
+    final newFile =
+        await file.copy(_path(preferencesDirectory, basename(file.path)));
+
+    _lastVisitedDirectory = file.parent;
+    return basename(newFile.path);
   }
 
   ///
   /// Serialize all the values
   Map<String, dynamic> get _serializePreferences => {
-        'pomodoroRunningImagePath': activeBackgroundImagePath,
+        'activeBackgroundImageFilename': _activeBackgroundImageFilename,
+        'pauseBackgroundImageFilename': _pauseBackgroundImageFilename,
+        'lastVisitedDirectory': _lastVisitedDirectory.path,
       };
 }
