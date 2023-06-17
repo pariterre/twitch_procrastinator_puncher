db923c626fd655c635d7b2a3ca38b6912ae53872
Pariterre <pariterre@hotmail.com>
2023-06-17 16:22:37 -0400

    Forced 1920x1080 window

--- main_program/lib/main.dart
+++ main_program/lib/main.dart
@@ -5,8 +5,20 @@ import 'package:common_lib/providers/pomodoro_status.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:twitch_procastinator_puncher/screens/main_screen.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(1920, 1080),
+    center: true,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   await declareAppDirectory();
   final appPreferences = await AppPreferences.factory();
   final participants = await Participants.factory(
